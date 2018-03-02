3bb3e6cc760aa58eb4f43dd3c3a2ecd17f144a6c
Brandon Donnelson <branflake2267@gmail.com>
2018-03-01 16:52:23 -0800

    Adding license.

--- src/main/java/com/sencha/gxt/desktopapp/client/service/ProfileServiceProvider.java
+++ src/main/java/com/sencha/gxt/desktopapp/client/service/ProfileServiceProvider.java
@@ -1,1 +1,38 @@
+/**
+ * Sencha GXT 1.0.0-SNAPSHOT - Sencha for GWT
+ * Copyright (c) 2006-2018, Sencha Inc.
+ *
+ * licensing@sencha.com
+ * http://www.sencha.com/products/gxt/license/
+ *
+ * ================================================================================
+ * Commercial License
+ * ================================================================================
+ * This version of Sencha GXT is licensed commercially and is the appropriate
+ * option for the vast majority of use cases.
+ *
+ * Please see the Sencha GXT Licensing page at:
+ * http://www.sencha.com/products/gxt/license/
+ *
+ * For clarification or additional options, please contact:
+ * licensing@sencha.com
+ * ================================================================================
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * ================================================================================
+ * Disclaimer
+ * ================================================================================
+ * THIS SOFTWARE IS DISTRIBUTED "AS-IS" WITHOUT ANY WARRANTIES, CONDITIONS AND
+ * REPRESENTATIONS WHETHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE
+ * IMPLIED WARRANTIES AND CONDITIONS OF MERCHANTABILITY, MERCHANTABLE QUALITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, DURABILITY, NON-INFRINGEMENT, PERFORMANCE AND
+ * THOSE ARISING BY STATUTE OR FROM CUSTOM OR USAGE OF TRADE OR COURSE OF DEALING.
+ * ================================================================================
+ */
 package com.sencha.gxt.desktopapp.client.service;import com.google.gwt.user.client.ui.HasWidgets;import com.sencha.gxt.desktopapp.client.ProfilePresenter;import com.sencha.gxt.desktopapp.client.servicebus.ServiceProvider;public class ProfileServiceProvider implements ServiceProvider<ProfileServiceRequest> {  private ProfilePresenter profilePresenter;  private HasWidgets hasWidgets;  public ProfileServiceProvider(ProfilePresenter profilePresenter, HasWidgets hasWidgets) {    this.profilePresenter = profilePresenter;    this.hasWidgets = hasWidgets;  }  @Override  public void onServiceRequest(ProfileServiceRequest profileServiceRequest) {    profilePresenter.go(hasWidgets);  }}
